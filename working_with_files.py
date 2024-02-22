61a0febcf818e12fa8fcba6c9f64af0c0fcb632f
fee3fitri <110148438+fee3fitri@users.noreply.github.com>
2024-02-22 14:51:56 -0800

    Add working with files

---
+++ working_with_files.py
@@ -0,0 +1,6 @@
+import os
+
+# os.remove("guests.txt")
+# os.rename("first_draft.txt", "finished_masterpiece.txt")
+print(os.path.exists("finished_masterpiece.txt"))
+print(os.path.exists("reading_writing_files.py"))
